0830ec188d1afddc437291ee6ae7f3326a1ccf3b
PedroACoura <pedroacoura@gmail.com>
2021-09-21 14:19:15 -0300

    Correção de printf

--- Atividade/src/Versao3/LayoutDoPrograma.java
+++ Atividade/src/Versao3/LayoutDoPrograma.java
@@ -156,7 +156,7 @@ public class LayoutDoPrograma {
                     System.out.printf("%n************************************************" +
                             "%nLista de usuários cadastrados no programa:%n");
                     for (Usuario usuario1 : Usuario.dataBaseParaSelecionarUsuarios) {
-                        System.out.printf("%d  - nome do cliente %s - cpf %s  - data de nascimento %s %n", i, usuario1.nomeDoBeneficiario, usuario1.cpf, usuario1.datadenascimento);
+                        System.out.printf("%d  - nome do cliente %s - cpf %d  - data de nascimento %s %n", i, usuario1.nomeDoBeneficiario, usuario1.cpf, usuario1.datadenascimento);
                         i += 1;
                     }
                     System.out.println("************************************************");
---
+++ Atividade/src/Versao3/Main.java
@@ -0,0 +1,9 @@
+package Versao3;
+
+public class Main {
+    public static void main(String[] args) {
+        LayoutDoPrograma layoutDoPrograma = new LayoutDoPrograma();
+        layoutDoPrograma.entradaDoPrograma();
+    }
+
+}
